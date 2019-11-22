64fad332d667ef94922e3f71d4686c134bc5fbe9
attiilla <58055483+attiilla@users.noreply.github.com>
2019-11-22 01:12:30 -0300

    Add files via upload

--- Index.cpp
+++ Index.cpp
@@ -17,21 +17,55 @@ struct freq{
 Index::Index() : base_(map<string,vector<freq> >()){
 }
 
-Index::Index(string c): Index(){
-    vector<string> vc = Conv(c);
-    Form(vc);
-    vector<freq> vf;
-    for(unsigned long int i=0;i<vc.size();i++){
-        if (base_.count(vc[i])==0){
-            vf[0].file = c;
-            vf[0].t=1;
-            //mymap.insert ( std::pair<char,int>('a',100) )
-            base_.insert(pair<string,vector<freq> >(vc[i],vf));
-        } else {
-            base_[vc[i]].t=
+void Index::Form(vector<string>& vc)
+{
+    for(unsigned long int i=0; i<vc.size(); i++)
+    {
+        for(unsigned int j=0; j<vc[i].size(); j++)
+        {
+            if(vc[i].begin()[j]=='+'||vc[i].begin()[j]=='-'||vc[i].begin()[j]=='.'||vc[i].begin()[j]==','||
+               vc[i].begin()[j]=='?'||vc[i].begin()[j]=='!'||vc[i].begin()[j]=='@'||vc[i].begin()[j]=='\\'||
+               vc[i].begin()[j]==':'||vc[i].begin()[j]==';'||vc[i].begin()[j]=='*'||vc[i].begin()[j]=='('||
+               vc[i].begin()[j]==')'||vc[i].begin()[j]=='['||vc[i].begin()[j]==']'||vc[i].begin()[j]=='{'||
+               vc[i].begin()[j]=='}')
+            {
+                vc[i].erase(vc[i].begin()+j);
+            }
         }
     }
 }
+
+Index::Index(string c): Index(){
+    vector<string> vc = Conv(c);
+    Form(vc);
+    vector<vector<freq> > vvf;
+    vector<freq> auxv;
+    freq auxf;
+    auxf.file = c;
+    auxf.t = 1;
+    //para cada i-ésima palavra
+    for(unsigned long int i=0;i<vc.size();i++){
+            //se a palavra não está no mapa
+        if (base_.count(vc[i])==0){
+            vvf[i][0].file = c;
+            vvf[i][0].t=1;
+            //mymap.insert ( std::pair<char,int>('a',100) )
+            base_[vc[i]] = vvf[i];
+        } else {
+            base_[vc[i]][0].t++;
+        }
+    }
+}
+
+/*auxf.file=c;
+auxf.t=1
+aux.push_back(auxf)
+vvf.push_back(auxv)
+*/
+
+
+
+
 
 vector<string> Index::Conv(string c){
     ifstream arq;
@@ -44,16 +78,3 @@ vector<string> Index::Conv(string c){
     return vc;
 }
 
-void Form(vector<string>& vc)
-{
-    for(unsigned long int i=0; i<vc.size(); i++)
-    {
-        for(int j=0; j<vc[i].size(); j++)
-        {
-            if(vc[i].begin()[j]=='+'||'-'||'.'||','||'?'||'!'||'@')
-            {
-                vc[i].erase(vc[i].begin()+j);
-            }
-        }
-    }
-}
--- Index.h
+++ Index.h
@@ -19,18 +19,16 @@ public:
     //constrói um índice com os valores do arquivo de nome citado
     Index(string c);
     //constrói um índice com todos os valores dos arquivos do vetor varq
-    Index(vector<string> vc);
     //converte o arquivo de nome c em um vetor de strings
     vector<string> Conv(string c);
     //formata um vetor de strings para retirar símbolo
-    vector<string> Form(vector<string> vc);
+    void Form(vector<string>& vc);
     //adiciona as informações do aquivo arq a um índice pre-existente
     void AddDataFromFile(ifstream arq);
     //adiciona as informações do aquivo arq a um índice pre-existente
     void AddDataFromFile(vector<ifstream> varq);
-
     //destrutor
-    ~Index();
+    //~Index();
 private:
     map<string,vector<freq> > base_;
     //adiciona uma variável do tipo frequência(freq) ao vetor de uma palavra que já existe no índice
